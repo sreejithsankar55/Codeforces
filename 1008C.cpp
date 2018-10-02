81172f792fc8700ca82e91b75a038dfe49ce8b2b
chandu <32795376+chanduch1999@users.noreply.github.com>
2018-10-02 14:31:18 +0530

    Update 1008C.cpp
    Made the code look short and beautiful

--- 1008C.cpp
+++ 1008C.cpp
@@ -2,26 +2,21 @@
 
 #include    <bits/stdc++.h>
 
-#define 	ll 	long long
-#define 	ld 	long double
-#define 	pb 	push_back
 #define 	ff 	first
 #define 	ss 	second
 #define 	mp 	make_pair
-#define 	mod 1000000007
-#define 	pi 	3.141592653589793
 
 using namespace std;
 
 int main()
 {	
-    ll n,b,j=0,i=0,c=0;
+    long long n,b,j=0,i=0,c=0;
     cin>>n;
-    vector < ll > v,v1;
-	for ( ll k = 0 ; k < n ; k++ )
+    vector < long long > v,v1;
+	for ( long long k = 0 ; k < n ; k++ )
 	{	
 		cin>>b;
-		v.pb(b);
+		v.push_back(b);
 	}
 	sort(v.begin(),v.end());
 	v1=v;
