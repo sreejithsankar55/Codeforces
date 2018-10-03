825cf184000a60d8017745e44df204e1b34196ad
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-10-03 18:12:46 +0530

    Update 1030C.cpp

--- 1030C.cpp
+++ 1030C.cpp
@@ -10,40 +10,40 @@
 #define ios ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 
-int main(){
-	
-	ll n,sum=0,k,c=0,p=0;
-	cin>>n;
-	string ss;
-	cin>>ss;
-	string s;
-	loop(i,0,n-1)
-	{
-		if(ss[i]=='0')
-			p++;
-		else
-			s+=ss[i];
-	}
-	if(p==n)
-		return cout<<"YES",0;
-	n=s.size();
-	k=(n/2)+1;
-	loop(i,1,9*k)
-	{
-		c=0;
-		sum=0;
-		loop(j,0,n-1)
-		{
-			sum+=(s[j]-'0');
-			if(sum==i)
-			{
-				sum=0;
-				c++;
-				if(j==n-1&&c>1)
-					return cout<<"YES",0;
-			}
-		}
-	}
+int main() {
 
-	return	cout<<"NO",0;
+    ll n,sum=0,k,c=0,p=0;
+    cin>>n;
+    string ss;
+    cin>>ss;
+    string s;
+    loop(i,0,n-1)
+    {
+        if(ss[i]=='0')
+            p++;
+        else
+            s+=ss[i];
+    }
+    if(p==n)
+        return cout<<"YES",0;
+    n=s.size();
+    k=(n/2)+1;
+    loop(i,1,9*k)
+    {
+        c=0;
+        sum=0;
+        loop(j,0,n-1)
+        {
+            sum+=(s[j]-'0');
+            if(sum==i)
+            {
+                sum=0;
+                c++;
+                if(j==n-1&&c>1)
+                    return cout<<"YES",0;
+            }
+        }
+    }
+
+    return	cout<<"NO",0;
 }
