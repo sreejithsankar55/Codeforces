fcfdccfa01e87a68f1f08d9e13d773675e76fa34
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-10-05 18:33:41 +0530

    Create 1047B.cpp

---
+++ 1047B.cpp
@@ -0,0 +1,30 @@
+/* (: Sreejith Codes :) */
+
+#include    <bits/stdc++.h>
+
+#define 	ll 	long long
+#define 	ld 	long double
+#define 	pb 	push_back
+#define 	ff 	first
+#define 	ss 	second
+#define 	mp 	make_pair
+#define 	pi 	3.141592653589793
+#define 	ios ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+int main()
+{
+	ios;
+	ll n,mi=0;
+	cin>>n;
+	while(n--)
+	{
+		ll x,y;
+		cin>>x>>y;
+		if((x+y)>mi)
+			mi=x+y;
+	}
+	
+	
+	return 0;
+}
