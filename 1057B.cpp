6e2bf7770bc832234d1390085910bffc4ebc3347
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-10-05 18:35:46 +0530

    Create 1057B.cpp

---
+++ 1057B.cpp
@@ -0,0 +1,40 @@
+/* (: Sreejith Codes :) */
+
+#include    <bits/stdc++.h>
+
+#define 	ll 	long long
+#define 	ld 	long double
+#define 	pb 	push_back
+#define 	ff 	first
+#define 	ss 	second
+#define 	mp 	make_pair
+#define 	pi 	3.141592653589793
+#define 	ios ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+	ll l,r,ce=0,co=0;
+	cin>>l>>r;
+	
+	for(ll i = l ; i <= r ; i++)
+	{
+		if(i&1)
+			co++;
+		else 
+			ce++;
+		
+	}
+	if(co!=ce)
+		cout<<"NO";
+	else
+	{
+		cout<<"YES"<<endl;
+		
+		for(ll i = l ; i <  r ; i+=2)
+		{
+			cout<<i<<""<<(i+1)<<endl;
+		}
+	} 
+	
+	return 0;
+}
