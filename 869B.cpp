e8d3564369afca7949e4ebec4d00d424189a1b0d
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-10-05 18:37:22 +0530

    Create 869B.cpp

---
+++ 869B.cpp
@@ -0,0 +1,32 @@
+/* (: Sreejith Codes :) */
+
+#include    <bits/stdc++.h>
+
+#define 	ll 	long long
+#define 	ld 	long double
+#define 	pb 	push_back
+#define 	ff 	first
+#define 	ss 	second
+#define 	mp 	make_pair
+#define 	pi 	3.141592653589793
+#define 	ios ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+int main()
+{
+    ll a,b,c=1;
+    cin>>a>>b;
+
+    for(ll i = b ; i > a ; i--)
+    {
+        c*=(i%10);
+        c%=10;
+        if(c==0)
+           
+    }
+
+    cout<<c;
+
+
+    return 0;
+}
