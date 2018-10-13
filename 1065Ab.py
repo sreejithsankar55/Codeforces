d26860d1f663eadc3de6a35e8456732d206cd047
arjunn3007@gmail.com <36272519+arjunn3007@users.noreply.github.com>
2018-10-13 17:12:31 +0530

    Update 1065Ab.py

--- 1065Ab.py
+++ 1065Ab.py
@@ -2,7 +2,7 @@ http://codeforces.com/contest/1065/problem/A
 
 t=int(input())
 while(t--):
-	s,a,b,c=map(int(input().split))
+	s,a,b,c=map(int,(input().split()))
 	ru=a*c
 	p=(s//ru)*a
 	res=(s//ru)*b
