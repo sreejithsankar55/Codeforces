eb94f2719ceeb9601c84fccdb19b34de7eb47e9f
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-10-14 21:44:36 +0530

    Create 1064C.cpp

---
+++ 1064C.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define vsort(v) sort(v.begin(),v.end())
+#define setpr fixed<<setprecision
+#define gl(a) getline(cin,a)
+#define ss second
+#define ll long long
+#define ld long double
+using namespace std;
+
+int main()
+{
+	ll n;
+	cin>>n;
+	string s;
+	cin>>s;
+	
+	map < char , ll > ma;
+	for(ll i = 0 ; i < n;i++)
+	{
+		ma[s[i]]++;
+	}
+	for(auto it=ma.begin();it!=ma.end();it++)
+	{
+		ll c=it->ss;
+		while(c--)
+		{
+			cout<<it->ff;
+		}
+	}
+	
+
+	return 0;
+}
