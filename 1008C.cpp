357c468e6b762824aa7c0975bc67665d1f7ac035
Aakash K O <aakashko1998@gmail.com>
2019-10-01 20:17:06 +0530

    Update 1008C.cpp

--- 1008C.cpp
+++ 1008C.cpp
@@ -5,15 +5,16 @@
 #define 	ff 	first
 #define 	ss 	second
 #define 	mp 	make_pair
+#define         ll      long long
 
 using namespace std;
 
 int main()
 {	
-    long long n,b,j=0,i=0,c=0;
+    ll n,b,j=0,i=0,c=0;
     cin>>n;
-    vector < long long > v,v1;
-	for ( long long k = 0 ; k < n ; k++ )
+    vector < ll > v,v1;
+	for ( ll k = 0 ; k < n ; k++ )
 	{	
 		cin>>b;
 		v.push_back(b);
